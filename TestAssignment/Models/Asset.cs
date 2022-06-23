406281f120703314d3b53e293188e3deac563855
Hedeus <94357857+Hedeus@users.noreply.github.com>
2022-06-23 18:49:11 +0300

    Получение тестовых данных и заготовка классов описания криптовалюты

---
+++ TestAssignment/Models/Asset.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+namespace TestAssignment.Models
+{
+    internal class Asset
+    {
+        [JsonProperty("asset_id")]
+        public string AssetId { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("website")]
+        public string Website { get; set; }
+        [JsonProperty("price")]
+        public double Price { get; set; }
+        [JsonProperty("volume_24h")]
+        public double Volume24h { get; set; }
+        [JsonProperty("change_1h")]
+        public double Change1h { get; set; }
+        [JsonProperty("change_24h")]
+        public double Change24h { get; set; }
+        [JsonProperty("change_7d")]
+        public double Change7d { get; set; }
+    }
+}
--- TestAssignment/ViewModels/MainWindowViewModel.cs
+++ TestAssignment/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,58 @@
-﻿using TestAssignment.ViewModels.Base;
+﻿using System.IO;
+using System.Net;
+using System.Windows.Input;
+using TestAssignment.Infrastructure.Commands;
+using TestAssignment.ViewModels.Base;
 
 namespace TestAssignment.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        #region Title : string - Заголовок вікна
+        private string _Title = "Головне вікно";
+        public string Title { get => _Title; set => Set(ref _Title, value); }
+        #endregion
+
+        #region Status : string - Стан
+        private string _Status = "Готово";
+        public string Status { get => _Status; set => Set(ref _Status, value); }
+        #endregion
+        /*-------------------------------------Методи-------------------------------------------*/
+
+        private string LoadData()
+        {
+            string url = "https://www.cryptingup.com/api/assets?size=10";
+            //string url = "https://api.coincap.io/v2/assets";
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            string response;
+            using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+            {
+                response = streamReader.ReadToEnd();
+            }
+            return response;
+        }
+
+        /*-------------------------------------Команди-------------------------------------------*/
+        #region MyRegion
+
+        private ICommand _LoadDataCommand;
+        public ICommand LoadDataCommand => _LoadDataCommand
+            ??= new LambdaCommand(OnLoadDataCommandExecuted, CanLoadDataCommandExecute);
+        private bool CanLoadDataCommandExecute() => true;
+        private void OnLoadDataCommandExecuted()
+        {
+            Status = "Кнопку натиснуто";
+            string response = LoadData();
+        }
+
+        #endregion
+
+        /*-----------------------------------Конструктор-------------------------------------------*/
+
+        public MainWindowViewModel()
+        {
+
+        }
     }
 }
