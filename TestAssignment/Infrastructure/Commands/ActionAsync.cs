8f20a945a44dc1b3865bab13543b0ede92176350
Hedeus <94357857+Hedeus@users.noreply.github.com>
2022-06-24 17:13:36 +0300

    Вывод на экран основной информации

---
+++ TestAssignment/Infrastructure/Commands/ActionAsync.cs
@@ -0,0 +1,7 @@
+﻿using System.Threading.Tasks;
+
+namespace TestAssignment.Infrastructure.Commands
+{
+    internal delegate Task ActionAsync();
+    internal delegate Task ActionAsync<in T>(T parameter);
+}
--- TestAssignment/ViewModels/MainWindowViewModel.cs
+++ TestAssignment/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,11 @@
-﻿using System.IO;
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
 using System.Net;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using TestAssignment.Infrastructure.Commands;
+using TestAssignment.Models;
 using TestAssignment.ViewModels.Base;
 
 namespace TestAssignment.ViewModels
@@ -17,33 +21,78 @@ namespace TestAssignment.ViewModels
         private string _Status = "Готово";
         public string Status { get => _Status; set => Set(ref _Status, value); }
         #endregion
+
+        #region AssetsList : AssetsRoot - Сторінка зі списком криптовалюти
+        private AssetsRoot _AssetsList = new AssetsRoot();
+        public AssetsRoot AssetsList { get => _AssetsList; set => Set(ref _AssetsList, value); }
+        #endregion
+
+        #region CanLoadData : bool - Можливысть завантаження даних
+        private bool _CanLoadData = true;
+        public bool CanLoadData {  get => _CanLoadData; set => Set(ref _CanLoadData, value); }
+        #endregion
         /*-------------------------------------Методи-------------------------------------------*/
 
-        private string LoadData()
+        private AssetsRoot LoadData()
         {
             string url = "https://www.cryptingup.com/api/assets?size=10";
             //string url = "https://api.coincap.io/v2/assets";
             HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
-            HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-            string response;
-            using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+            try
             {
-                response = streamReader.ReadToEnd();
+                HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                string response;
+                using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+                {
+                    response = streamReader.ReadToEnd();
+                }
+                AssetsRoot assets = JsonConvert.DeserializeObject<AssetsRoot>(response);
+                return assets;
             }
-            return response;
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException("Не вдалось отримати даны про приптовалюту", ex);
+            }
         }
+
+        private async Task<AssetsRoot> LoadDataAsync()
+        {            
+            string url = "https://www.cryptingup.com/api/assets?size=10";
+            //string url = "https://api.coincap.io/v2/assets";
+            if (AssetsList.Next != "")
+                url += "&&start=" + AssetsList.Next;
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            try
+            {
+                HttpWebResponse httpWebResponse = (HttpWebResponse)(await httpWebRequest.GetResponseAsync());
+                string response;
+                using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+                {
+                    response = streamReader.ReadToEnd();
+                }
+                AssetsRoot assets = JsonConvert.DeserializeObject<AssetsRoot>(response);                
+                return assets;
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException("Не вдалось отримати даны про приптовалюту", ex);
+            }            
+        }
+
 
         /*-------------------------------------Команди-------------------------------------------*/
         #region MyRegion
 
         private ICommand _LoadDataCommand;
         public ICommand LoadDataCommand => _LoadDataCommand
-            ??= new LambdaCommand(OnLoadDataCommandExecuted, CanLoadDataCommandExecute);
-        private bool CanLoadDataCommandExecute() => true;
-        private void OnLoadDataCommandExecuted()
+            ??= new LambdaCommandAsync(OnLoadDataCommandExecuted, CanLoadDataCommandExecute);
+        private bool CanLoadDataCommandExecute() => CanLoadData;
+        private async Task OnLoadDataCommandExecuted()
         {
+            //CanLoadData = !CanLoadData;
             Status = "Кнопку натиснуто";
-            string response = LoadData();
+            AssetsList = await LoadDataAsync();
+            //CanLoadData = !CanLoadData;
         }
 
         #endregion
